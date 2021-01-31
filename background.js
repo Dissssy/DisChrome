2e6d33cc46e606796903bca0a7f5701a3ace4b4a
Dissssy <ethanconaway@yahoo.com>
2021-01-31 13:04:20 -0500

    minor fixes
    removed unnecessary permission, added comments and whatnot. hopefully theyll accept it to the webstore now

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
+//function call to get data from chrome
 function getValue(callback) {
   chrome.storage.sync.get(['SAVEDATA'], callback);
 };
 
+//helper function to create and fulfill post request to discord webhook
 function postit(webhook, content, postname, postpfp) {
 	var request = new XMLHttpRequest();
 	request.open("POST", webhook);
@@ -14,34 +16,46 @@ function postit(webhook, content, postname, postpfp) {
 	request.send(JSON.stringify(params));
 };
 
+//main code, run in the chrome function callback
 getValue(function (value) {
+	
+	//attempt to parse the data recieved from chrome, if its not valid, assume the user is new and cancel execution
 	try{
 		USERDATA = JSON.parse(value.SAVEDATA);
 	}catch{
 		return false;
 	};
+	
+	//attempt to read the idcount, if that fails, something is wrong, cancel execution
 	if(USERDATA.hasOwnProperty("idcount")){
 		var idcount = parseInt(USERDATA.idcount);
 	}else{
 		return false;
 	};
+	
+	//loop through every id
 	for(i = 1; i < idcount + 1; i++){
 		var url = USERDATA[i].url;
 		var name = USERDATA[i].name;
+		//name is optional, handle if that field is empty by removing "to"
 		if(name != ""){
 			var id = "Post to " + USERDATA[i].id;
 		}else{
 			var id = "Post"
 		};
+		//create context menu item if this id has a url
 		if(url != ""){
 			chrome.contextMenus.create({"id": "" + i, "title": id, "contexts": ["image", "link", "selection", "video", "audio"]});
 		};
 	};
+	//add listener for the context menu items
 	chrome.contextMenus.onClicked.addListener(function(clickData){
+		//get all of the click data (for readability)
 		clicked = clickData.menuItemId
 		var clickedurl = USERDATA[clicked].url;
 		var clickedname = USERDATA[clicked].name;
 		var clickedpfp = USERDATA[clicked].pfp;
+		//create and fill string to post
 		stringtopost = "";
 		if(clickData.hasOwnProperty('srcUrl')){
 			stringtopost = stringtopost + " " + clickData.srcUrl
@@ -50,9 +64,11 @@ getValue(function (value) {
 		}else if(clickData.hasOwnProperty('selectionText')){
 			stringtopost = stringtopost + " " + clickData.selectionText
 		};
+		//check if string to post is below 2k characters (discord hard limit) if it is below that, call post helper function to post content
 		if (stringtopost.length <= 2000) {
 			postit(clickedurl, stringtopost, clickedname, clickedpfp);
 		}else{
+			//if string is too long, alert user that it is too long and dont post
 			alert(stringtopost.length + " characters is too long, discord only allows 2000 maximum");
 		}
 	});
--- options.js
+++ options.js
@@ -1,6 +1,9 @@
+//function call to get data from chrome
 function getValue(callback) {
 	chrome.storage.sync.get(["SAVEDATA"], callback);
 }
+
+//helper function to create generic form objects
 function createForm(defaulturl, defaultid, defaultname, defaultpfp){	
 	//create form
 	var form = document.createElement("form");
@@ -17,7 +20,6 @@ function createForm(defaulturl, defaultid, defaultname, defaultpfp){
 	
 	//create id input
 	var idinput = document.createElement("input");
-	idinput.setAttribute("style", "margin-left:5px");
 	idinput.setAttribute("autofill", "false");
 	idinput.setAttribute("type", "text");
 	idinput.setAttribute("id", "id" + i);
@@ -27,7 +29,6 @@ function createForm(defaulturl, defaultid, defaultname, defaultpfp){
 	
 	//create name input
 	var nameinput = document.createElement("input");
-	nameinput.setAttribute("style", "margin-left:5px");
 	nameinput.setAttribute("autofill", "false");
 	nameinput.setAttribute("type", "text");
 	nameinput.setAttribute("id", "name" + i);
@@ -37,7 +38,6 @@ function createForm(defaulturl, defaultid, defaultname, defaultpfp){
 	
 	//create pfp input
 	var pfpinput = document.createElement("input");
-	pfpinput.setAttribute("style", "margin-left:5px");
 	pfpinput.setAttribute("autofill", "false");
 	pfpinput.setAttribute("type", "text");
 	pfpinput.setAttribute("id", "pfp" + i);
@@ -52,59 +52,78 @@ function createForm(defaulturl, defaultid, defaultname, defaultpfp){
 	form.appendChild(pfpinput);
 	document.getElementsByTagName("body")[0].getElementsByTagName("div")[0].appendChild(form);
 }
+
+//main code, run in the chrome function callback
 getValue(function (value){
+	
+	//attempt to parse the data recieved from chrome, if its not valid, assume the user is new
 	try{
 		USERDATA = JSON.parse(value.SAVEDATA);
 	}catch{
-		alert(value.SAVEDATA + " is not a valid JSON string \n this may be your first time using the extension, if so, welcome \n you can ignore this. \n if this is not your first time, i recommend you copy this and try to salvage the info to save some time");
 		USERDATA = JSON.parse("{}");
 	}
-
+	
+	//attempt to read the idcount (used to keep track of how many inputs exist) if its not found, set it to 0
 	if(USERDATA.hasOwnProperty("idcount")){
 		var idcount = parseInt(USERDATA.idcount);
 	}else{
 		var idcount = 0;
 	}
+	
+	//loop to get default data, then create prefilled input forms
 	for(i = 1; i < idcount + 1; i++){
+		
 		//getting default values
+		//check if the current form id property exists
 		if(USERDATA.hasOwnProperty(i)){
+			//check if current form has url property
 			if(USERDATA[i].hasOwnProperty("url")){
 				defaulturl = USERDATA[i].url;
 			}else{
 				defaulturl = ""
 			}
+			//check if current form has id property
 			if(USERDATA[i].hasOwnProperty("id")){
 				defaultid = USERDATA[i].id;
 			}else{
 				defaultid = ""
 			}
+			//check if current form has name property
 			if(USERDATA[i].hasOwnProperty("name")){
 				defaultname = USERDATA[i].name;
 			}else{
 				defaultname = ""
 			}
+			//check if current form has pfp property
 			if(USERDATA[i].hasOwnProperty("pfp")){
 				defaultpfp = USERDATA[i].pfp;
 			}else{
 				defaultpfp = ""
 			}
 		}else{
+			//if this form id doesnt exist just set the values to blank
 			defaulturl = ""
 			defaultid = ""
 			defaultname = ""
 			defaultpfp = ""
 		}
+		//call create form helper with default values
 		createForm(defaulturl, defaultid, defaultname, defaultpfp);
 	}
-
+	
+	//form handler
 	$(function(a){
+		//activate when any button is pressed
 		$(":submit").click(function(){
+			//get which button is pressed and handle each button differently
 			var pressed = $(this).attr('id');
+			//add button is pressed, create new blank form, increment different form handler values
 			if(pressed == "ADDFORM"){
 				createForm("", "", "", "");
 				i++;
 				idcount = idcount + 1;
 			}
+			//subtract button is pressed, delete last form in list, decrement different form handler values, or alert that there are no more forms to remove
 			if(pressed == "SUBTRACTFORM"){
 				if(document.getElementsByTagName("form").length > 0){
 					document.getElementsByTagName("form")[document.getElementsByTagName("form").length - 1].remove();
@@ -114,8 +133,11 @@ getValue(function (value){
 					alert("NOTHING TO REMOVE");
 				};
 			};
+			//submit button is pressed
 			if(pressed == "SUBMITFORMS"){
+				//set Form idcount key value to the form id count
 				var Form = {"idcount" : idcount};
+				//loop through each form on the page, read the data and write it to the form id value under form
 				for(i = 1; i < idcount + 1; i++){
 					Form[i] = {};
 					Form[i].url = $('#url' + i).val();
@@ -123,8 +145,9 @@ getValue(function (value){
 					Form[i].name = $('#name' + i).val();
 					Form[i].pfp = $('#pfp' + i).val();
 				};
+				//turn Form object into a json string, save that to the SAVEDATA value in chrome storage, alert user that the settings have been saved, reload extension
 				chrome.storage.sync.set({'SAVEDATA': JSON.stringify(Form)}, function(){
-					//alert(JSON.stringify(Form) + "settings saved")
+					alert("settings saved")
 					chrome.runtime.reload(); 
 				});
 			};
